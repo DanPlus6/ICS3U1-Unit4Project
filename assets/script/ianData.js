513bd8846c678bb791fe49184528a4c858e248d1
Ian Choy <boidiamond69420@gmail.com>
2026-05-21 22:06:43 +0800

    Synchronized personal post secondary research to remote

---
+++ assets/script/ianData.js
@@ -0,0 +1,26 @@
+'use strict';
+
+schoolNames[7] = 'The University of Hong Kong: Department of Data and Systems Engineering';
+cities[7] = 'Pok Fu Lam';
+provinces[7] = 'Hong Kong';
+countries[7] = 'China';
+schoolPictures[7] = 'https://hkuvn.edu.vn/wp-content/uploads/2024/10/about-img2.png';
+mapPictures[7] = 'https://goglobal.ubc.ca/sites/default/files/styles/square_600/public/2024-08/University%20of%20Hong%20Kong.jpg?itok=TdKCS6ra';
+programNames[7] = 'Bachelor of Engineering in Data and Systems Engineering';
+lengthsOfPrograms[7] = 4;
+programDescriptions[7] = 'Students learn the fundamentals of system analysis and computiting disciplines, like operational research, mathematical and data-driven optimization, along with system simulation and modeling. Students are also introduced to artificial intelligence and intelligence system technologies.';
+coop[7] = true;
+admissionCoursesNeeded[7] = 'English<br>Chinese<br>Mathematics<br>Liberal Sciences / Citizenship and Social Development<br>Biology / Chemistry / Combined Science / Physics'
+admissionAveragesNeeded[7] = 75;
+domesticTuitions[7] = 8164.56;
+internationalTuitions[7] = 43254.04;
+academicCostDetails[7] = 'Learning materials and activities';
+academicCosts[7] = 17.52;
+livingCostDetails[7] = 'Living on campus and Average life of student living in Hong Kong';
+livingCosts[7] = 13482.74;
+interestingFacts[7] = '<ol><li>University of Hong Kong is the top university in Asia and the 16th best university in the world</li><li>University of Hong Kong started as the Hong Kong College of Medicine for Chinese in 1887</li><li>University of Hong Kong is the oldest university in Hong Kong</li></ol>';
+primarySources1[7] = 'https://www.dase.hku.hk/teaching-and-learning/current-students/bachelor-of-engineering-in-data-and-systems-engineering-beng-dase';
+primarySources2[7] = 'https://admissions.hku.hk/apply/jupas';
+primarySources3[7] = 'https://admissions.hku.hk/fees-and-scholarships/fees';
+secondarySources1[7] = 'https://laurenlovesnoodles.blogspot.com/2013/05/8-things-to-know-about-hku.html';
+secondarySources2[7] = 'https://kids.kiddle.co/University_of_Hong_Kong';
