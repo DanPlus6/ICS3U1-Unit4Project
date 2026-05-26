8b0dbc3ce96bb118c2205485640a93124d1ad82b
Ian Choy <boidiamond69420@gmail.com>
2026-05-26 21:52:15 +0800

    Imputed countOccurrences function into its own js file

---
+++ assets/script/function/countOccurrances.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * Finds the amount of times this event happens in an array
+ * @param {*} anArray an array to search through
+ * @param {*} lowestInput the lowest range -- or if there is no highest range, if the number is equal
+ * @param {*} highestInput the highest range -- 0 if there is no highest range
+ * @returns the amount of times it occurs 
+ */
+function countOccurrances(anArray, lowestInput, highestInput){
+    let countAmount = 0;
+    //check if there is no highest input
+    if (highestInput == 0){
+        //traverse through the array to count the amount of times the array element is equal to the lowest bound
+        for (let i; i < anArray.length; i++){
+            if (anArray[i] == lowestInput){
+                countAmount++;
+            }
+        }
+    }
+    else {
+         //traverse through the array to count the amount of times the array element is inside the ranges
+        for (let i; i < anArray.length; i++){
+            if (anArray[i] >= lowestInput && anArray[i] <= highestInput){
+                countAmount++;
+            }
+        }
+    }
+    return countAmount;
+}
