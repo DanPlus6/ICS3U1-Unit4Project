ab3c11b0d40cbf92170163ccdeef5a74ce48d8f4
Ian Choy <boidiamond69420@gmail.com>
2026-05-26 21:51:55 +0800

    Imputed updateData function into its own independent js file

--- assets/script/traversal.js
+++ assets/script/traversal.js
@@ -4,36 +4,6 @@
 let currentIndex = 0;
 
 /**
- * Updates the arrays with the current index variable
- */
-function updateData(updateIndex){
-    schoolNames[updateIndex];
-    cities[updateIndex];
-    provinces[updateIndex];
-    countries[updateIndex];
-    schoolPictures[updateIndex];
-    mapPictures[updateIndex];
-    programNames[updateIndex];
-    lengthsOfPrograms[updateIndex];
-    programDescriptions[updateIndex];
-    coop[updateIndex];
-    admissionCoursesNeeded[updateIndex];
-    admissionAveragesNeeded[updateIndex];
-    domesticTuitions[updateIndex];
-    internationalTuitions[updateIndex];
-    academicCostDetails[updateIndex];
-    academicCosts[updateIndex];
-    livingCostDetails[updateIndex];
-    livingCosts[updateIndex];
-    interestingFacts[updateIndex];
-    primarySources1[updateIndex];
-    primarySources2[updateIndex];
-    primarySources3[updateIndex];
-    secondarySources1[updateIndex];
-    secondarySources2[updateIndex];
-}
-
-/**
  * Increases the current index by 1 to show the next school
  */
 function nextSchool(){
