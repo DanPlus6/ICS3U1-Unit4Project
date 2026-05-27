1e786a344c2b37032985581b08d71033f8b7bc07
Ian Choy <boidiamond69420@gmail.com>
2026-05-27 22:28:51 +0800

    Optimized rangeSearch function and made comments consistant

--- assets/script/function/rangeSearch.js
+++ assets/script/function/rangeSearch.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import { pop } from './pop.js';
+
 /**
  * Finds the indexes of an array within the highest and lowest input
  * @param {Array} anArray an array to search through
@@ -8,23 +10,27 @@
  * @returns 0 if there is no index between these ranges, an array with the indexes if there are elements within these ranges
  */
 function tuitionRanges(anArray, lowestInput, highestInput){
-    //contains the amount of times the element is inside the ranges
-    let indexCount = countOccurrances(anArray, lowestInput, highestInput);
-    //check if there is no element inside the range
-    if (indexCount == 0){
-        return 0;
-    }
-    //contains the indexes inside the ranges
-    let rangeIndexes = new Array(indexCount);
-    //secondary iterator that traverses through the rangeIndexes array
+    // contains the amount of indexes within the ranges
+    let indexCount = 0
+    // contains the indexes inside the ranges
+    let rangeIndexes = new Array(anArray.length);
+    // secondary iterator that traverses through the rangeIndexes array
     let k = 0;
-    //traverse through the array to find the indexes that are inside the ranges
+    // traverse through the array to find the indexes that are inside the ranges
     for (let i; i < anArray.length; i++){
         //find if the current element is inside the ranges
         if (anArray[i] >= lowestInput && anArray[i] <= highestInput){
             rangeIndexes[k] = i;
             k++;
         }
+    }
+
+    if (indexCount == 0){
+        return -1;
+    }
+
+    while (indexCount--){
+        pop(rangeIndexes);
     }
     return rangeIndexes;
 }
