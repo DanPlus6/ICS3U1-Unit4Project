56dfa07136c77dd7e76772988cbcaa84541688f5
dave <d4nplus6@gmail.com>
2026-05-27 10:25:20 -0400

    Optimized searchAllMatches function, fixed formatting and added proper comments

--- assets/script/function/searchAllMatches.js
+++ assets/script/function/searchAllMatches.js
@@ -1,23 +1,36 @@
 'use strict';
 
+import { pop } from './pop.js';
+
+/**
+ * Searches for all occurences of a value in an array
+ * @param {Array} arr the input array
+ * @param {*} searchVal the search value
+ * @returns {number[]} all indices where value is found
+ * 
+ * By: Ian Choy
+ */
 export function searchAllMatches(arr, searchVal) {
-    //contains the amount of times the search value is found in the array
-    let count = countOccurrances(arr, searchVal, 0);
-    //search if search value was not found
-    if (count == 0){
-        return 0;
-    }
-    //contains the indexes of the original array that contains the search value
-    let newArr = new Array(count);
-    //secondary iterator to traverse through newArr
+    /** track the amount of times the search value is found in the array */
+    let count = 0;
+    /** array to contain the indexes of the original array that contains the search value */
+    let indexes = new Array(arr.length);
+    /** secondary iterator to traverse through newArr */
     let k = 0;
-    //traverse through the original array to find the indexes of what contains search value
-    for (i = 0; i < arr.length; i++){
-        //search if current element is equal to search value
-        if (arr[i] == searchVal){
-            newArr[k] = i
-            k++;
+
+    // traverse through the original array to find the indexes of what contains search value
+    for (i = 0; i < arr.length; i++) {
+        // search if current element is equal to search value
+        if (arr[i] == searchVal) {
+            indexes[k] = i;
+            ++k;
+            ++count;
         }
     }
-    return newArr;
+
+    if (count == 0) return -1;
+
+    while (count--) pop(arr);
+
+    return indexes;
 }
