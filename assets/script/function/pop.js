4c8465a83be70822461f86bbfefd77d70143ea06
dave <d4nplus6@gmail.com>
2026-05-27 09:54:54 -0400

    Moved theoretical implementation of Array.pop method to a standalone function

---
+++ assets/script/function/pop.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Pops the last element of an array
+ * @param {Array} arr the array to operate on
+ * @returns Last element of array that is popped
+ */
+export function pop(arr) {
+    const lElem = arr[arr.length-1];
+    --arr.length;
+    return lElem;
+}
--- assets/script/index.js
+++ assets/script/index.js
@@ -1,7 +1,1 @@
 'use strict';
-
-Array.prototype.mpop = function() {
-    let lastElem = this[this.length-1];
-    --this.length;
-    return lastElem;
-}
