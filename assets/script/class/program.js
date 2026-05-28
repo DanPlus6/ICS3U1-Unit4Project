151564e0710e0b15da14e7965c24c30324d68fc5
dave <d4nplus6@gmail.com>
2026-05-28 10:21:58 -0400

    Updated university program class definition and added json-friendly construction/parsing methods

--- assets/script/class/program.js
+++ assets/script/class/program.js
@@ -1,13 +1,38 @@
 'use strict';
 
 export class program {
-    constructor(
-        schoolName, city, country, schoolPic, mapPic, programName,
-        programLen, programDesc, hasCoop, admissionCourses, admissionAvg,
-        domesticTuition, internationalTuition, academicCostDetails, academicCosts,
-        livingCostDetails, livingCosts, interestingFacts, primarySource1,
-        primarySource2, primarySource3, secondarySource1, secondarySource2
-    ) {
-        
+    /** class to store research for a single university program */
+    constructor(data) {
+        this.legacyIndex = data.legacyIndex;
+        this.legacyFile = data.legacyFile;
+        this.schoolName = data.schoolName;
+        this.city = data.city;
+        this.province = data.province;
+        this.country = data.country;
+        this.schoolPicture = data.schoolPicture;
+        this.mapPicture = data.mapPicture;
+        this.programName = data.programName;
+        this.lengthOfProgram = data.lengthOfProgram;
+        this.programDescription = data.programDescription;
+        this.hasCoop = data.hasCoop;
+        this.admissionCoursesNeeded = data.admissionCoursesNeeded;
+        this.admissionAverageNeeded = data.admissionAverageNeeded;
+        this.domesticTuition = data.domesticTuition;
+        this.internationalTuition = data.internationalTuition;
+        this.academicCostDetails = data.academicCostDetails;
+        this.academicCost = data.academicCost;
+        this.livingCostDetails = data.livingCostDetails;
+        this.livingCost = data.livingCost;
+        this.interestingFacts = data.interestingFacts;
+        this.primarySources = data.primarySources || [];
+        this.secondarySources = data.secondarySources || [];
+    }
+
+    static fromJson(data) {
+        return new program(data);
+    }
+
+    static fromJsonArray(programs) {
+        return programs.map((programData) => program.fromJson(programData));
     }
 }
