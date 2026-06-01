11fc01aa2547b116e6be07e987d87f3167499a19
Ian Choy <boidiamond69420@gmail.com>
2026-06-01 13:04:08 +0800

    removed redundent variable

--- assets/script/function/searchAllMatches.js
+++ assets/script/function/searchAllMatches.js
@@ -11,19 +11,16 @@ import { pop } from './pop.js';
  * By: Ian Choy
  */
 export function searchAllMatches(arr, searchVal) {
-    /** track the amount of times the search value is found in the array */
-    let count = 0;
     /** array to contain the indexes of the original array that contains the search value */
     let indexes = new Array(arr.length);
     /** secondary iterator to traverse through newArr */
-    let k = 0;
+    let count = 0;
 
     // traverse through the original array to find the indexes of what contains search value
     for (i = 0; i < arr.length; i++) {
         // search if current element is equal to search value
         if (arr[i] == searchVal) {
-            indexes[k] = i;
-            ++k;
+            indexes[count] = i;
             ++count;
         }
     }
