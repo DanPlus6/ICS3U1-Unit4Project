fc48e78af354ae07cf1022036813365305d6d736
Ian Choy <boidiamond69420@gmail.com>
2026-06-02 11:36:34 +0800

    Added constants for HTML elements

--- assets/script/school.js
+++ assets/script/school.js
@@ -13,6 +13,27 @@ const urlParams = new URLSearchParams(window.location.search);
 /** index for traversing through the programs */
 let idx = urlParams.get('idx');
 
+// get the HTML elements
+const PROGRAM_NAME = document.getElementById('programName');
+const LENGTH_OF_PROGRAM = document.getElementById('lengthOfProgram');
+const PROGRAM_DESCRIPTION = document.getElementById('programDescription');
+const HAS_COOP = document.getElementById('hasCoop');
+const ADMISSION_COURSES_NEEDED = document.getElementById('admissionCoursesNeeded');
+const ADMISSION_AVERAGE_NEEDED = document.getElementById('admissionAverageNeeded');
+const DOMESTIC_TUITION = document.getElementById('domesticTuition');
+const INTERNATIONAL_TUTION = document.getElementById('internationalTuition');
+const ACADEMIC_COST_DETAILS = document.getElementById('academicCostDetails');
+const ACADEMIC_COST = document.getElementById('academicCost');
+const LIVING_COST_DETAILS = document.getElementById('livingCostDetails');
+const LIVING_COST = document.getElementById('livingCost');
+const INTERESTING_FACTS = document.getElementById('interstingFacts');
+const PRIMARY_SOURCE1 = document.getElementById('primarySource1');
+const PRIMARY_SOURCE2 = document.getElementById('primarySource2');
+const PRIMARY_SOURCE3 = document.getElementById('primarySource3');
+const SECONDARY_SOURCE1 = document.getElementById('secondarySource1');
+const SECONDARY_SOURCE2 = document.getElementById('secondarySource2');
+const SECONDARY_SOURCE3 = document.getElementById('secondarySource3');
+
 /** 
  * Updates current webpage to display info of current program
  * @param {Program} program current program
