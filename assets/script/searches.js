643fa3544502fc23258d4818dc07c6614da18131
Ian Choy <boidiamond69420@gmail.com>
2026-06-03 01:47:03 +0800

    Fixed missing .js extension in imports

--- assets/script/searches.js
+++ assets/script/searches.js
@@ -1,16 +1,16 @@
 'use strict';
 
 // Import the functions from the function toolbox
-import { addArrays } from './function/addArrays';
-import { rangeSearch } from './function/rangeSearch';
-import { removeDuplicates } from './function/removeDuplicates';
-import { searchString } from './function/searchString';
-import { searchAllMatches } from './function/searchAllMatches';
-import { sortArray } from ',/function/sortArray';
-import { Program } from './class/Program';
-import { pop } from './function/pop';
-import { countOccurrances } from './function/countOccurrances';
-import { getPrograms } from './function/getPrograms';
+import { addArrays } from './function/addArrays.js';
+import { rangeSearch } from './function/rangeSearch.js';
+import { removeDuplicates } from './function/removeDuplicates.js';
+import { searchString } from './function/searchString.js';
+import { searchAllMatches } from './function/searchAllMatches.js';
+import { sortArray } from ',/function/sortArray.js';
+import { Program } from './class/Program.js';
+import { pop } from './function/pop.js';
+import { countOccurrances } from './function/countOccurrances.js';
+import { getPrograms } from './function/getPrograms.js';
 
 //get the HTML elements
 const TXT_INPUT = document.getElementById('txt-input');
