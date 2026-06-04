9d07ea0134ea4c464da973bbbb9a22a6869a6153
dave <d4nplus6@gmail.com>
2026-06-04 00:26:01 -0400

    Added function to create labelled paragraphs for program information fields

---
+++ assets/script/function/makeDetails.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * Creates a labelled paragraph for one field of program information
+ * @param {string} label field label
+ * @param {string} value field value
+ * @returns {HTMLParagraphElement} paragraph containing the label and value
+ */
+export function makeDetail(label, value) {
+    const p = makeElem({tag:'p', htmlContent:`${label}: `});
+    const span = makeElem({tag:'span', htmlContent:value});
+    p.append(span);
+
+    return p;
+}
