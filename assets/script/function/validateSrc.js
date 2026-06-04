9bb1de82a76dd2714c267e89bf84cdccd9797562
dave <d4nplus6@gmail.com>
2026-06-04 18:16:27 -0400

    Added early invalidation condition when source is empty to validateSrc function

--- assets/script/function/validateSrc.js
+++ assets/script/function/validateSrc.js
@@ -13,6 +13,12 @@
  * @returns {bool} whether or not source is valid
  */
 export function validateSrc(src, mode="url") {
+    // if source is empty, invalidate immediately
+    if (!src) {
+        resolve(false);
+        return;
+    }
+
     if (mode === "url") { // check if validation mode is url structural check
         try { // attempt to construct url using provided source to structurally validate the source
             const url = new URL(src);
@@ -26,12 +32,6 @@ export function validateSrc(src, mode="url") {
     } else if (mode === "img") { // check if validation mode is image src check
         // attempt to construct new image using source to validate it
         return new Promise((resolve) => {
-            // if source is empty, invalidate immediately
-            if (!src) {
-                resolve(false);
-                return;
-            }
-
             /** img object to construct for validation */
             const img = new Image();
             // resolve promise based on whether image loads successfully or path is broken/missing/blocked
