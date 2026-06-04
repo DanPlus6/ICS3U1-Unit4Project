470cf27dc2dc80f45071d0231e1acdd7394448f0
dave <d4nplus6@gmail.com>
2026-06-04 00:12:42 -0400

    Added optional specification of class name and id to makeElem function

--- assets/script/function/makeElem.js
+++ assets/script/function/makeElem.js
@@ -1,19 +1,28 @@
 'use strict';
 
 /**
- * Function to create a html element
- * @param {HTMLElementType} tag type of html tag/element to create
- * @param {string} htmlContent optional innerHTML content to assign to element
+ * Creates html element optionally with a value
+ * @param {Object} args object of arguments passed
+ * @param {HTMLElementType} args.tag type of html tag/element to create
+ * @param {string} [args.className] optional classname to assign to the element
+ * @param {string} [args.id] optional id to assign to the element
+ * @param {string} [args.htmlContent] optional innerHTML content to assign to element
  * @returns {HTMLElement} target to created html element
  */
-export function makeElem(tag, htmlContent=null) {
-    /**
-     * target to HTML element to create
+export function makeElem({tag, className, id, htmlContent}) {
+    /** 
+     * target to created html element
      * @type {HTMLElement}
      */
     const elem = document.createElement(tag);
-    // if innerHTML content is specified, assign it
+    
+    // check if class name was specified to assign it
+    if (className) elem.className = className;
+    // check if id was specified to assign it
+    if (id) elem.id = id;
+    // check if innerHTML content was specified to assign it
     if (htmlContent) elem.innerHTML = htmlContent;
+
 
     return elem;
 }
