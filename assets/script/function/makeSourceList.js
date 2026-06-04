56e8482420c344d432b7122dafcb6f918ba3b55a
dave <d4nplus6@gmail.com>
2026-06-04 00:26:59 -0400

    Added function to create list of research source links for a program

---
+++ assets/script/function/makeSourceList.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * Creates a list of research source links
+ * @param {string[]} sources array of source urls
+ * @returns {HTMLUListElement} list containing source links
+ * 
+ * By: David Fu
+ */
+export function makeSourceList(sources) {
+    const sourceList = makeElem({tag:'ul'});
+
+    for (const source of sources) {
+        const sourceItem = makeElem({tag:'li'});
+        const sourceLink = makeElem({tag:'a', href:source, htmlContent:source});
+        sourceItem.append(sourceLink);
+        sourceList.append(sourceItem);
+    }
+
+    return sourceList;
+}
