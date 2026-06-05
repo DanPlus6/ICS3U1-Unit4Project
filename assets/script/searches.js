998199e147b3706d9ca59d3664f32f3e489f92db
dave <d4nplus6@gmail.com>
2026-06-05 00:16:30 -0400

    Added functions for co-op and tuition toggling in searches page

--- assets/script/searches.js
+++ assets/script/searches.js
@@ -4,12 +4,13 @@ import { Program } from "./class/Program.js";
 
 
 //get the HTML elements
-const SRCH_IPT = document.getElementById('search-input');
-const T_MIN = document.getElementById('tuition-min');
-const T_MAX = document.getElementById('tuition-max');
-const P_LEN = document.getElementById('program-length');
-const BTN_TT = document.getElementById('btn-swap-tuition');
-const BTN_COOP = document.getElementById('btn-toggle-coop');
+const SRCH_IPT = document.getElementById("search-input");
+const T_MIN = document.getElementById("tuition-min");
+const T_MAX = document.getElementById("tuition-max");
+const P_LEN = document.getElementById("program-length");
+const BTN_TT = document.getElementById("btn-swap-tuition");
+const BTN_COOP = document.getElementById("btn-toggle-coop");
+const BTN_SRCH = document.getElementById("btn-search");
 
 /** 
  * array to store search results to render
@@ -24,11 +25,43 @@ let hasCoop = false;
  * flag to track if to filter programs by international tuition
  * `true` = international, `false` = domestic
  */
-let intCoop = false;
+let intTuition = false;
+
 
 /** callback to execute the search */
 function execSearch() {
+    
+}
 
+/** callback to swap tuition mode */
+function swapTuition() {
+    if (intTuition) {
+        intTuition = false;
+        BTN_TT.innerHTML = "Has Co-op: Yes";
+    } else {
+        intTuition = true;
+        BTN_TT.innerHTML = "Has Co-op: No";
+    }
+}
+
+/** callback to swap */
+function toggleCoop() {
+    if (hasCoop) {
+        hasCoop = false;
+        BTN_COOP.innerHTML = "Has Co-op: Yes";
+    } else {
+        hasCoop = true;
+        BTN_COOP.innerHTML = "Has Co-op: No";
+    }
 }
 
 
+/** initialization callback  */
+function init() {
+    BTN_TT.addEventListener("click", swapTuition);
+    BTN_COOP.addEventListener("click", toggleCoop);
+    BTN_SRCH.addEventListener("click", execSearch);
+
+
+}
+document.addEventListener("DOMContentLoaded", init);
